2c31f06f75023a120e1c98653c9290af004ad113
JaronArmiger <jaron.armiger@gmail.com>
2020-08-23 17:02:17 -0500

    implement genre_list controller aciton and view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,11 +1,36 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+const async = require('async');
 
 exports.genre_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre list');
+  Genre.find()
+    .sort([['name', 'ascending']])
+    .exec(function(err, list_genres) {
+      if (err) return next(err);
+      res.render('genre_list', { title: 'Genre List', genre_list: list_genres} );
+    });
 };
 
-exports.genre_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+  async.parallel({
+  	genre: function(callback) {
+  	  Genre.findById(req.params.id)
+  	    .exec(callback);
+  	},
+  	genre_books: function(callback) {
+  	  Book.find({ 'genre': req.params.id })
+  	    .exec(callback);
+  	},
+  }, function(err, results) {
+  	if (err) return next(err);
+  	if (results.genre==null) {
+  	  const error = new Error('Genre not found');
+  	  err.status = 404;
+  	  return next(err);
+  	}
+  	res.render('genre_detail', { title: 'Genre Detail', 
+  	  genre: results.genre, genre_books: results.genre_books });
+  });
 };
 
 exports.genre_create_get = function(req, res) {
