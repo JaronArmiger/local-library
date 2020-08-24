17da82fa41d460cd6c6a91352f5c05605dc27c34
JaronArmiger <jaron.armiger@gmail.com>
2020-08-24 12:27:30 -0500

    write book_update_get action

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -144,8 +144,37 @@ exports.book_delete_post = function(req, res) {
   res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
-exports.book_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = function(req, res, next) {
+  async.parallel({
+    book: function(callback) {
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    authors: function(callback) {
+      Author.find(callback);
+    },
+    genres: function(callback) {
+      Genre.find(callback);
+    },
+  }, function(err, results) {
+       if (err) return next(err);
+       if (results.book==null) {
+         const err = new Error('Book not found');
+         err.status = 404;
+         return next(err);
+       }
+       // mark selected genres as checked
+       for (let all_g_iter; all_g_iter < results.genres.length; all_g_iter++) {
+         if (results.genres[all_g_iter]._id.toString()==results.book.genre[book_g_iter]._id.toString()) {
+           results.genres[all_g_iter].checked='true';
+         }
+       }
+       res.render('book_form', { title: 'Update Book', authors: results.authors,
+        genres: results.genres, book: results.book
+       });
+  });
 };
 
 exports.book_update_post = function(req, res) {
